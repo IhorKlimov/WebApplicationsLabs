4c7a58b8f3f9512337db2416e0cdada36377a94c
Ihor Klimov <ihor.klimov@ssa.group>
2021-09-26 14:36:26 +0300

    Add lab 2

---
+++ lab2/script.js
@@ -0,0 +1,107 @@
+window.onload = function () {
+    let borderColor = getCookie("borderColor");
+    if (borderColor.length > 0) {
+        changeBorderColors(borderColor);
+    }
+    let reversedCookie = getCookie("reversed");
+    if (reversedCookie.length > 0) {
+        alert("Reading cookie " + reversedCookie + ". Press OK to clear the cookies");
+        document.cookie = "reversed=;";
+        alert("Cookies were cleared. Press OK to reload the document");
+        window.location.reload(false);
+        return;
+    }
+    swapSecondAndFifthParagraphChildren();
+    calculateRectangleArea(12, 45);
+}
+
+function swapSecondAndFifthParagraphChildren() {
+    const two = document.getElementById("two");
+    const five = document.getElementById("five");
+
+    const twoClone = two.cloneNode(true);
+    two.replaceChildren(...five.children);
+    five.replaceChildren(...twoClone.childNodes);
+}
+
+function calculateRectangleArea(width, height) {
+    const area = width * height;
+
+    const four = document.getElementById("four");
+    const paragraph = document.createElement("p");
+    const text = document.createTextNode("Calculated area of a rectangle: " + area);
+    paragraph.appendChild(text);
+    four.appendChild(paragraph);
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function reverseRectangleArea() {
+    const number = document.forms["numberInput"]["number"].value;
+    let reversed = reverseString(number);
+    alert(reversed);
+    document.cookie = "reversed=" + reversed;
+    return false;
+}
+
+function setBorderColor() {
+    const color = document.forms["borderColor"]["color"].value;
+    alert(color);
+    document.cookie = "borderColor=" + color;
+    changeBorderColors(color);
+}
+
+function changeBorderColors(color) {
+    document.getElementsByClassName("one")[0].style.border = "1px solid " + color;
+    document.getElementsByClassName("two")[0].style.border = "1px solid " + color;
+    document.getElementsByClassName("three")[0].style.border = "1px solid " + color;
+    document.getElementsByClassName("four")[0].style.border = "1px solid " + color;
+    document.getElementsByClassName("five")[0].style.border = "1px solid " + color;
+    document.getElementsByClassName("six")[0].style.border = "1px solid " + color;
+    document.getElementsByClassName("seven")[0].style.border = "1px solid " + color;
+}
+
+function showFormInputs() {
+    document.querySelectorAll("form input").forEach(value => {
+        applyCSSFor(value);
+    });
+    document.querySelectorAll("form button").forEach(value => {
+        applyCSSFor(value);
+    });
+}
+
+function applyCSSFor(element) {
+    element.style.visibility = "visible";
+}
+
+function disableCSSFor(element) {
+    element.style.visibility = "hidden";
+    document.cookie = "CSSInstruction=;";
+}
+
+function saveCSSInstruction(element) {
+    document.cookie = "CSSInstruction=" + "value.style.visibility = \"visible\"";
+    applyCSSFor(element);
+}
+
+function reverseString(str) {
+    let result = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
+}
